0634cfe6b0aac528029ed6745d441bc3bc463a9c
Alex Crouzen <alexander@crouzen.com>
2019-06-25 13:12:55 +0100

    Update 2

---
+++ src/main/java/com/crouzen/mytutorial/MyTutorial.java
@@ -0,0 +1,49 @@
+package com.crouzen.mytutorial;
+
+import com.crouzen.mytutorial.blocks.FirstBlock;
+import com.crouzen.mytutorial.blocks.ModBlocks;
+import com.crouzen.mytutorial.setup.ClientProxy;
+import com.crouzen.mytutorial.setup.IProxy;
+import com.crouzen.mytutorial.setup.ServerProxy;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("mytutorial")
+public class MyTutorial {
+
+    public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MyTutorial() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().register(new FirstBlock());
+        }
+        @SubscribeEvent
+        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().register(new BlockItem(ModBlocks.FIRSTBLOCK, new Item.Properties()).setRegistryName("firstblock"));
+        }
+    }
+}
---
+++ src/main/java/com/crouzen/mytutorial/blocks/ModBlocks.java
@@ -0,0 +1,8 @@
+package com.crouzen.mytutorial.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+    @ObjectHolder("mytutorial:firstblock")
+    public static FirstBlock FIRSTBLOCK;
+}
